e9211f0e153effad741d44eafda80fd3d2fa2c4a
root <afsalonline01mail.com>
2025-10-08 14:02:31 +0700

    Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,39 +1,53 @@
 import { motion } from "framer-motion";
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
+  const typedEl = useRef(null);
 
-  
+  useEffect(() => {
+    const typed = new Typed(typedEl.current, {
+      strings: ["Afsal Maulana", "a Web Developer", "a UI/UX Enthusiast"],
+      typeSpeed: 70,
+      backSpeed: 50,
+      backDelay: 1500,
+      loop: true,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className='flex flex-col justify-center items-center mt-28 md:mt-5 lg:5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        <div className="flex flex-col justify-center items-center mt-28 md:mt-5 lg:5">
+          <div className="w-5 h-5 rounded-full bg-[#2563EB]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
         <div className="mt-28 md:mt-5 lg:5">
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Afsal Maulana</span>
+            Hi, I'm <span className="text-[#2563EB]" ref={typedEl}></span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop modern, user <br/>
-            interfaces and web applications
+            As a Web Developer with a degree in Information Systems, <br />
+            I specialize in creating modern and professional web solutions
           </p>
         </div>
       </div>
 
       <ComputersCanvas />
 
-      
-
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
               animate={{
                 y: [0, 24, 0],
@@ -43,7 +57,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
         </a>
