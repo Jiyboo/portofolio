cc4eaf0b5ce2f730f6ad9f78ade09f0a9625c984
arfa <113402712+Jiyboo@users.noreply.github.com>
2025-10-10 21:44:52 +0700

    Update Resume.jsx

--- src/id/components/Resume.jsx
+++ src/id/components/Resume.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
     <div
       className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
     >
-      {}
+
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl overflow-y-auto"
@@ -29,13 +29,10 @@ const Contact = () => {
             className="w-full h-auto rounded-lg"
           />
         </div>
-
-        {}
         <div className="hidden sm:block mt-2 w-full h-[400px] overflow-y-auto border border-gray-700 rounded-lg bg-white p-4">
           <CVCode />
         </div>
 
-        {}
         <a
           href={cvFile}
           download="Afsal_Maulana_CV.pdf"
@@ -45,7 +42,6 @@ const Contact = () => {
         </a>
       </motion.div>
 
-      {}
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
         className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
