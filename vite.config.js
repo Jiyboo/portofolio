509efc8afdd151bd31c53172df09347c830c854a
Jiyboo <afsalonline01@gmail.com>
2026-06-16 23:35:26 +0700

    Update vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: "brotliCompress",
+      ext: ".br",
+    }),
+    viteCompression({
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+  ],
   server: {
     host: "0.0.0.0",
     port: 5173,
