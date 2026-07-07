925732bda216ba203d994d5178a37f14d234f6cc
Jiyboo <afsalonline01@gmail.com>
2026-07-07 21:59:26 +0700

    Remove Image Tech

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,29 +7,10 @@ import github from "./github.png";
 import link from "./link.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import gsap from './tech/gsap.png'
-import smaw from './tech/smaw.png'
-import java from './tech/java.png'
-import framer from './tech/framer.png';
-import css from "./tech/css.png";
-import figma from "./tech/figma.png";
 import project5 from './project5.png';
 import project4 from './project4.png';
 import project3 from './project3.png';
 import project2 from './proj1.png'
-import mysql from './tech/mysql.png';
-import express from './tech/express.png';
-import aws from './tech/aws.png';
-import mui from './tech/mui.png'
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import threejs from "./tech/threejs.svg";
 
 export {
   logo,
