6041d230a7dd2d2a58ba62ed90084841f7715feb
Jiyboo <afsalonline01@gmail.com>
2026-07-07 21:21:37 +0700

    Update Cursor

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -2,17 +2,24 @@ import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 
 const CustomCursor = () => {
-  const cursorRef = useRef(null);
+  const dotRef = useRef(null);
+  const ringRef = useRef(null);
 
   useEffect(() => {
-    gsap.set(cursorRef.current, { xPercent: -50, yPercent: -50 });
+    gsap.set(dotRef.current, { xPercent: -50, yPercent: -50 });
+    gsap.set(ringRef.current, { xPercent: -50, yPercent: -50 });
 
     const moveCursor = (e) => {
-      gsap.to(cursorRef.current, {
+      gsap.to(dotRef.current, {
         x: e.clientX,
         y: e.clientY,
-        duration: 0.15,
-        ease: "power2.out",
+        duration: 0,
+      });
+      gsap.to(ringRef.current, {
+        x: e.clientX,
+        y: e.clientY,
+        duration: 0.25,
+        ease: "power3.out",
       });
     };
 
@@ -24,10 +31,16 @@ const CustomCursor = () => {
   }, []);
 
   return (
-    <div
-      ref={cursorRef}
-      className="fixed top-0 left-0 w-8 h-8 rounded-full bg-white pointer-events-none z-[9999] mix-blend-difference"
-    ></div>
+    <>
+      <div
+        ref={dotRef}
+        className="fixed top-0 left-0 w-2 h-2 bg-[#915eff] rounded-full pointer-events-none z-[10000] shadow-[0_0_8px_rgba(145,94,255,0.8)]"
+      ></div>
+      <div
+        ref={ringRef}
+        className="fixed top-0 left-0 w-10 h-10 border border-[#915eff]/60 bg-[#915eff]/5 rounded-full pointer-events-none z-[9999]"
+      ></div>
+    </>
   );
 };
 
